1e1960df9d2d5680728a93068618e2311f9d3e04
Josh Conley <joshsconley@gmail.com>
2015-07-23 01:33:58 -0700

    Added line chart example

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,8 +1,6 @@
 let fetch = require('./fetchData'),
     fs = require('fs');
 
-
-
-fetch.getNewRelicMetricData(5881048, [ 'System/CPU/IO Wait/percent', 'System/CPU/System/percent', 'System/CPU/User/percent' ], function(data) {
+/*fetch.getNewRelicMetricData(5881048, [ 'System/CPU/IO Wait/percent', 'System/CPU/System/percent', 'System/CPU/User/percent' ], function(data) {
   fs.writeFile(process.cwd() + '/public/data.json', JSON.stringify(data));
-});
+});*/
